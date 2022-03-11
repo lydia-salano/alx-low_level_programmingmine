fc2ce7f71a09f92943c9009701e56b88d32c16b2
Lydia Kimata <lydiakimata2@gmail.com>
2022-03-11 21:21:43 +0300

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,9 +13,9 @@ int main(void)
    char small;
 
    for (small = 'z'; small >= 'a'; small--)
+   {
        putchar(small);
-	
- putchar('\n');
-
-	return (0);
+       putchar('\n');
+   }
+    return (0);
 }
