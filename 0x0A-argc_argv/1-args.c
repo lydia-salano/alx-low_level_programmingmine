53d5777d9233a1acd5402f32623e9a9e0aa0c134
Lydia Kimata <lydiakimata2@gmail.com>
2022-04-03 10:45:57 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,8 +8,8 @@
  */
 int main(int argc, char *argv[])
 {
-(void) argv;
-	printf("%d\n", argc - 1);
-
+/* to suppress unused variable warning */
+argv = argv;
+printf("%d\n", argc - 1);
 return (0);
 }
