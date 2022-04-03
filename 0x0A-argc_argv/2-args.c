3bbc88f62239277e21930c36c3439987402c458b
Lydia Kimata <lydiakimata2@gmail.com>
2022-04-03 10:48:19 +0300

    Update 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,11 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	while (*argv)
-	{
-		(void) argc;
-		printf("%s\n", *argv);
-		argv++;
-	}
+	while (argc--)
+		printf("%s\n", *argv++);
 	return (0);
 }
