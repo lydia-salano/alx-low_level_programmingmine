b8a45e9032c40340456193e2da25e4bfe4b0e812
Lydia Kimata <lydiakimata2@gmail.com>
2022-04-21 23:49:24 +0300

    Update _printf.c

--- 0x11-printf/_printf.c
+++ 0x11-printf/_printf.c
@@ -1,109 +1,46 @@
 #include "main.h"
-#define BUF_SIZE 1024
-
 /**
-  * num_flags - counts number of flags total
-  * @term: our format tag
-  * Return: int, number of flags
-  */
-int num_flags(const char *term)
-{
-	const char *FLAGS = "+- 0123456789.#lh";
-	int i = 0, j = 0;
-
-	while (FLAGS[i])
-	{
-		if (FLAGS[i] == term[j])
-		{
-			i = 0;
-			j++;
-		}
-		i++;
-	}
-	return (j);
-}
-
-/**
-  * checkBuffer - checks if buffer is full
-  * @buf_idx: buffer's index
-  * @buffer: char * buffer to be printed
-  * Return: idx if not full, 0 if full to be refilled
-  */
-int checkBuffer(int buf_idx, char *buffer)
-{
-	if (buf_idx == BUF_SIZE)
-	{
-		write(1, buffer, buf_idx);
-		return (0);
-	}
-	return (buf_idx);
-}
-
-/**
-  * _printf - our version of printf
-  * @format: string of input, may have %tags that are replaced with args
-  * Return: int, length of total chars
-  */
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string
+ */
 int _printf(const char *format, ...)
 {
-	va_list ag;
-	int buf_idx = 0, i = 0, j, count = 0;
-	char *buffer, *temp;
-	spec_t specs[] = {
-		{'d', print_d},	{'i', print_d},	{'c', print_c},	{'s', print_s},
-		{'S', print_S},	{'u', print_u}, {'\0', NULL}
-	};
+	int (*pfunc)(va_list, flags_t *);
+	const char *p;
+	va_list arguments;
+	flags_t flags = {0, 0, 0};
 
-	if (!format)
+	register int count = 0;
+
+	va_start(arguments, format);
+	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-	buffer = malloc(sizeof(char) * BUF_SIZE);
-	if (!buffer)
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-	va_start(ag, format);
-	while (format[i])
+	for (p = format; *p; p++)
 	{
-		if (format[i] == '%' && format[i + 1] == '%')
+		if (*p == '%')
 		{
-			buffer[buf_idx] = '%';
-			buf_idx++;
-			count++;
-			buf_idx = checkBuffer(buf_idx, buffer);
-			i++;
-		}
-		else if (format[i] == '%' && format[i + 1])
-		{
-			j = num_flags(format + i + 1);
-			i += j + 1;
-			for (j = 0; specs[j].s; j++)
+			p++;
+			if (*p == '%')
 			{
-				if (format[i] == specs[j].s)
-				{
-					temp = specs[j].f(ag);
-					break;
-				}
+				count += _putchar('%');
+				continue;
 			}
-			if (!specs[j].s)
-				return (-1);
-			if (!temp)
-				return (-1);
-			for (j = 0; temp[j]; j++, buf_idx++)
-			{
-				buffer[buf_idx] = temp[j];
-				count++;
-				buf_idx = checkBuffer(buf_idx, buffer);
-			}
-		}
-		else
-		{
-			buffer[buf_idx] = format[i];
-			buf_idx++;
-			count++;
-			buf_idx = checkBuffer(buf_idx, buffer);
-		}
-		i++;
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*p);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
 	}
-	write(1, buffer, buf_idx);
-	va_end(ag);
-	free(buffer);
+	_putchar(-1);
+	va_end(arguments);
 	return (count);
 }
