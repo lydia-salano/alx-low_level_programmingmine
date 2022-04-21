b51c395701be266108af6dca43d32c556293fb61
Lydia Kimata <lydiakimata2@gmail.com>
2022-04-22 00:47:42 +0300

    Update _printf.c

--- 0x11-printf/_printf.c
+++ 0x11-printf/_printf.c
@@ -1,46 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
+
 /**
- * _printf - produces output according to a format
- * @format: format string containing the characters and the specifiers
- * Description: this function will call the get_print() function that will
- * determine which printing function to call depending on the conversion
- * specifiers contained into fmt
- * Return: length of the formatted output string
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
  */
-int _printf(const char *format, ...)
+int main(void)
 {
-	int (*pfunc)(va_list, flags_t *);
-	const char *p;
-	va_list arguments;
-	flags_t flags = {0, 0, 0};
+	int len, len2;
 
-	register int count = 0;
-
-	va_start(arguments, format);
-	if (!format || (format[0] == '%' && !format[1]))
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
-		return (-1);
-	for (p = format; *p; p++)
+	len = _printf("Let's print a simple sentence.\n");
+	len2 = printf("Let's print a simple sentence.\n");
+	fflush(stdout);
+	if (len != len2)
 	{
-		if (*p == '%')
-		{
-			p++;
-			if (*p == '%')
-			{
-				count += _putchar('%');
-				continue;
-			}
-			while (get_flag(*p, &flags))
-				p++;
-			pfunc = get_print(*p);
-			count += (pfunc)
-				? pfunc(arguments, &flags)
-				: _printf("%%%c", *p);
-		} else
-			count += _putchar(*p);
+		printf("Lengths differ.\n");
+		fflush(stdout);
+		return (1);
 	}
-	_putchar(-1);
-	va_end(arguments);
-	return (count);
+	return (0);
 }
