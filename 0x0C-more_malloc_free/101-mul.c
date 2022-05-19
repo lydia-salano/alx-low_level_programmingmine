9475c593e7de5719e3ff2fe38992e5c567c16395
Lydia Kimata <lydiakimata2@gmail.com>
2022-05-19 18:44:12 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,135 +1,99 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * int_calloc - calloc for ints
-  * @nmemb: n members
-  * @size: size array should be
-  * Return: int *
-  */
-int *int_calloc(int nmemb, unsigned int size)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	int *p, n;
-	/* malloc the memory, init to 0 */
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-		return (NULL);
-	for (n = 0; n < nmemb; n++)
-		p[n] = 0;
-	return (p);
-}
+	int i = 0;
 
-/**
-  * mult - multiply the things
-  * @res: int * result is stored in
-  * @n1: string num1
-  * @n2: string num2
-  * @len1: length of num1
-  * @len2: length of num2
-  * Return: void
-  */
-void mult(int *res, char *n1, char *n2, int len1, int len2)
-{
-	/* Declarations */
-	int i, j, f1, f2, sum;
-
-	for (i = len1 - 1; i >= 0; i--)
+	while (s[i])
 	{
-		sum = 0;
-		f1 = n1[i] - '0';
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			f2 = n2[j] - '0';
-			sum += res[i + j + 1] + (f1 * f2);
-			res[i + j + 1] = sum % 10;
-			sum /= 10;
-		}
-		if (sum > 0)
-			res[i + j + 1] += sum;
-	}
-	/* Take these magic numbers out */
-	for (i = 0; res[i] == 0 && i < len1 + len2; i++)
-	{}
-	if (i == len1 + len2)
-		_putchar('0');
-	for (; i < len1 + len2; i++)
-		_putchar(res[i] + '0');
-	_putchar('\n');
-}
-
-/**
-  * is_valid - is the number a valid one
-  * @num : char string num
-  * Return: int, 1 if true 0 if false
-  */
-int is_valid(char *num)
-{
-	int i;
-	/* input checking */
-	for (i = 0; num[i]; i++)
-	{
-		if (num[i] < '0' || num[i] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
+		i++;
 	}
 	return (1);
 }
-/**
-  * err - error function
-  * @status: error code to exit with
-  * Return: void
-  */
-void err(int status)
-{
-	/* Print "Error" and exit 98 */
-	_putchar('E');
-	_putchar('r');
-	_putchar('r');
-	_putchar('o');
-	_putchar('r');
-	_putchar('\n');
-	exit(status);
-}
-/**
-  * main - Entry Point
-  * @argc: argument count
-  * @argv: argument string array
-  * Return: 0
-  */
-int main(int argc, char **argv)
-{
-	int i, j, len1 = 0, len2 = 0;
-	int *res;
 
-	/* if there aren't two numbers as arguments */
-	if (argc != 3)
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		err(98);
+		i++;
 	}
-	for (i = 1; i < argc; i++)
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
-		/* if the two numbers are erroneous */
-		if (!(is_valid(argv[i])))
-			err(98);
-		/* find the length of each number */
-		if (i == 1)
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			for (j = 0; argv[i][j]; j++)
-				len1++;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (i == 2)
-		{
-			for (j = 0; argv[i][j]; j++)
-				len2++;
-		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	res = int_calloc(len1 + len2, sizeof(int));
-	if (res == NULL)
-		err(98);
-	/* actual multiply call */
-	mult(res, argv[1], argv[2], len1, len2);
-	free(res);
-	/* final return */
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
 	return (0);
 }
